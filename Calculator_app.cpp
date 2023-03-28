ac39f72609a3d850366e9fa2b4dd1ada0a914809
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-28 10:06:00 +0600

    Calculator_app_+,-,*,/;

---
+++ Calculator_app.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  float number1, number2, result;
+  string symbol;
+
+  cout << "Enter the 1st number: ";
+  cin >> number1;
+
+  cout << "Enter the 2nd number: ";
+  cin >> number2;
+
+  cout << "Enter The Symbol: ";
+  cin >> symbol;
+
+
+  cout << "The 1st Number is: " << number1 <<endl;
+  cout << "The 2nd Number is: " << number2 << endl;
+  cout << "The symbol is: " << symbol << endl;
+
+  if (symbol == "+") {
+    result = number1 + number2;
+  }
+
+  if (symbol == "-") {
+    result = number1 - number2;
+  }
+  
+  if (symbol == "*") {
+    result = number1 * number2;
+  }
+
+  if (symbol == "/") {
+    result = number1 / number2;
+  }
+
+  cout << "The result is: " << result << endl;
+
+  return 0;
+}
